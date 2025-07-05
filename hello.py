79f4bbff55ffe9aef8ed7594e62e97975a5ce04b
Neville Bokdawalla <nevillekb@gmail.com>
2025-07-05 21:42:05 +0530

    add comments

--- hello.py
+++ hello.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 
-# import modules
+# The above shebang is only required if yoyu run this script
+# directly from the command line as a program, for example
+# chmod +x hello.py && ./hello.py
+
+# import required modules
 import sys
 
+# main is not a special or reserved entry point by itself.
+# python uses this special pattern to define the entry point.
+# its just a regular function and has no special meaning to
+# the interpreter.
 def main():
     print('hello there,', sys.argv[1])
 
+# This is the real entry point for a python file thats run
+# directly - python3 <file name.py>
+# Python sets the buil-in variable __name__ to __main__
 if __name__ == '__main__':
     main()
